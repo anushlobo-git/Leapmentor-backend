237423c66191d7712d6acef412bce250961de8eb
Anush Lobo <anush.lobo@nineleaps.com>
2026-05-28 12:29:31 +0530

    wallet correction

--- repositories/wallet.repository.js
+++ repositories/wallet.repository.js
@@ -10,12 +10,17 @@ const findWalletByUser = (userId) => Wallet.findOne({ user: userId }).lean();
 
 const findWalletByUserMutable = (userId) => Wallet.findOne({ user: userId });
 
+const findWalletByUserAndRole = (userId, role) =>
+  Wallet.findOne({ user: userId, role }).lean();
 
+const createWallet = (data) => Wallet.create(data);
 
 module.exports = { 
     findAllWallets,
     findWalletByUserId,
     saveWallet,
     findWalletByUser,
-    findWalletByUserMutable
+    findWalletByUserMutable,
+    findWalletByUserAndRole,
+    createWallet,
 };
